5271828d83bf7e07e25e90236d7b4ad7261d1af0
pollmix <polboy777@gmail.com>
2019-06-25 11:21:58 +0600

    loading static files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -8,12 +9,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).send('page ot found');
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3005);
